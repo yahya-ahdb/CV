2097bb7b4aaf614de1a869a043a1cf4b3a57bcdf
yahya ahdab <eheyaahdab@gmail.com>
2023-05-21 12:22:32 -0700

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 const header = document.getElementById("header")
 const btn = document.getElementById("btnToggle")
+const landing = document.getElementById("landing")
+
+setTimeout(()=>{
+    landing.style.display = "none"
+},7000)
 
 btn.onclick = function (){
     header.classList.toggle("active")
